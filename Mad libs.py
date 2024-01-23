6a29207d9cb2f1144be74e635aa833fe7d87c185
Kendi <muriungikendileila@gmail.com>
2024-01-23 19:42:28 +0300

    Add files via upload

---
+++ Mad libs.py
@@ -0,0 +1,40 @@
+noun1 = input("Noun 1 : ")
+noun2 = input("Noun 2 : ")
+noun3 = input("Noun 3 : ")
+verb1 = input("verb 1 : ")
+adverb = input("Adverb: ")
+madlib = f"Keep reminding yourself that there's {noun1} and {noun2} for everything,\
+and that every step is {noun3}. So just keep {verb1} your best today, \
+and don't force what is yet suppose to fit in your life.It will happen {adverb} it's time." 
+
+print(madlib)
+
+adjective1 = input("Adjective 1 : ")
+noun4 = input("Noun 4 : ")
+noun5 = input("Noun 5 : ")
+noun6 = input("Noun 6 : ")
+noun7 = input("Noun 7 : ")
+noun8 = input("Noun 8 : ")
+verb2 = input("verb 2 : ")
+conjuction = input("Conjuction : ")
+adjective2 = input("Adjective 2: ")
+noun9 = input("Noun 9 : ")
+verb3 = input("verb 3 : ")
+adjective3 = input("Adjective 3: ")
+noun10= input("Noun 10 : ")
+verb4 = input("verb 4 : ")
+noun11 = input("Noun 11 : ")
+verb5 = input("verb 5 : ")
+noun12 = input("Noun 12 : ")
+noun13 = input("Noun 13 : ")
+verb6 = input("verb 6 : ")
+
+
+madlib2 = f"Being a programmer is {adjective1}. You get to create {noun4} and learn lots of {noun5} such as \
+Python, {noun6}, {noun7} to name a few. It's an unending {noun8}, full of enjoyment. I {verb2} programming \
+{conjuction} I get to be {adjective2} and build amazing software.As a {noun9} I get to {verb3} programs in \
+a variety of computer languages.There are times when it gets {adjective3} but {noun10} and discipline keeps \
+keeps one {verb4}. Fixing {noun11} on codes is what we also {verb5}. A programmer is sometimes referred to \
+as a {noun12} or a coder. I am a proud {noun13}. Happy {verb6}!"
+
+print(madlib2)
